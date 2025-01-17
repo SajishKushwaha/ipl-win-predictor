603fe2cd06fe2a125ee13d406f7c35c4547899b4
SajishKushwaha <sajishkumarmuz@gmail.com>
2025-01-18 00:18:34 +0530

    Updated Streamlit app

--- app.py
+++ app.py
@@ -51,7 +51,7 @@ if st.button('Predict Probabilities'):
     result=model.predict_proba(input_df)
     loss=result[0][0]
     win=result[0][1]
-  st.header(batting_team + ' - ' + str(round(win*100)) + "%")
+    st.header(batting_team + ' - ' + str(round(win*100)) + "%")
     st.header(bowling_team + ' - ' + str(round(loss*100)) + "%")
   
 
